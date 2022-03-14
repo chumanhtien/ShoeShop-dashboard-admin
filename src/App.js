dc9350a14a3d861668d142206a67c6a0215442fb
chumanhtien <tiencm194182@gmail.com>
2022-03-14 23:42:02 +0700

    first admin page commit original

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import "./responsive.css";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/productScreen";
 import CategoriesScreen from "./screens/CategoriesScreen";
@@ -13,42 +13,23 @@ import Login from "./screens/LoginScreen";
 import UsersScreen from "./screens/UsersScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
 import NotFound from "./screens/NotFound";
-import PrivateRouter from "./PrivateRouter";
-import { useDispatch, useSelector } from "react-redux";
-import { listProducts } from "./Redux/Actions/ProductActions";
-import { listOrders } from "./Redux/Actions/OrderActions";
 
 function App() {
-  const dispatch = useDispatch();
-
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
-
-  useEffect(() => {
-    if (userInfo && userInfo.isAdmin) {
-      dispatch(listProducts());
-      dispatch(listOrders());
-    }
-  }, [dispatch, userInfo]);
-
   return (
     <>
       <Router>
-        <Switch>
-          <PrivateRouter path="/" component={HomeScreen} exact />
-          <PrivateRouter path="/products" component={ProductScreen} />
-          <PrivateRouter path="/category" component={CategoriesScreen} />
-          <PrivateRouter path="/orders" component={OrderScreen} />
-          <PrivateRouter path="/order/:id" component={OrderDetailScreen} />
-          <PrivateRouter path="/addproduct" component={AddProduct} />
-          <PrivateRouter path="/users" component={UsersScreen} />
-          <PrivateRouter
-            path="/product/:id/edit"
-            component={ProductEditScreen}
-          />
-          <Route path="/login" component={Login} />
-          <PrivateRouter path="*" component={NotFound} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>} exact />
+          <Route path="/products" element={<ProductScreen/>} />
+          <Route path="/category" element={<CategoriesScreen/>} />
+          <Route path="/orders" element={<OrderScreen/>} />
+          <Route path="/order" element={<OrderDetailScreen/>} />
+          <Route path="/addproduct" element={<AddProduct/>} />
+          <Route path="/users" element={<UsersScreen/>} />
+          <Route path="/product/:id/edit" element={<ProductEditScreen/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Routes>
       </Router>
     </>
   );
